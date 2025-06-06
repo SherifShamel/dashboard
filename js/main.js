bedb0108f60584a96c7ff7292b078b5346f96a45
sherif <sherif_shamel15@yahoo.com>
2025-06-06 19:42:50 +0300

    added some more images, enhanced code for display() function and applied search() feature

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var productPriceInput = document.getElementById("productPrice");
 var productCategoryInput = document.getElementById("productCategory");
 var productDescriptionInput = document.getElementById("productDescription");
 var productImageInput = document.getElementById("productImage");
+var searchInput = document.getElementById("search");
 
 
 
@@ -14,7 +15,7 @@ if (localStorage.getItem("products") == null) {
 } else {
     productList = JSON.parse(localStorage.getItem("products"))
 
-    display();
+    display(productList);
 }
 
 // addProduct() function is called from line 48 in index.html
@@ -24,37 +25,52 @@ function addProduct() {
         price: productPriceInput.value,
         category: productCategoryInput.value,
         description: productDescriptionInput.value,
-        image: "image",
+        image: `assets/${productImageInput.files[0].name}`,
     }
     productList.push(product);
     localStorage.setItem("products", JSON.stringify(productList));
 
-    display();
+    display([productList]);
     // clearInputs();
 }
 
-function display() {
+function display(arr) {
     var cartona = "";
-    for (var i = 0; i < productList.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         cartona += `<div class="col-md-2 py-4">
                         <div class="item">
-                            <img src="assets/meal-1.jpg" class="w-100" alt="">
-                            <h2 class="h4">Name: ${productList[i].code}</h2>
-                            <p>Price: ${productList[i].price}</p>
-                            <p>Category: ${productList[i].category}</p>
-                            <p>Description: ${productList[i].description}</p>
+                            <img src="${arr[i].image}" class="w-100" alt="">
+                            <h2 class="h4">Name: ${arr[i].code}</h2>
+                            <p>Price: ${arr[i].price}</p>
+                            <p>Category: ${arr[i].category}</p>
+                            <p>Description: ${arr[i].description}</p>
                             <button onClick="deleteProduct(${i})" class="btn btn-outline-danger w-100">Delete <i class="fas fa-trash"></i></button>
                             <button class="btn btn-outline-warning my-2 w-100">Update <i class="fas fa-pen"></i></button>
                         </div>
                     </div>`
     }
-    document.getElementById("myRow").innerHTML = cartona;
+    if (cartona == "") {
+        document.getElementById("myRow").innerHTML = `<h3 class="text-center"> Not Found</h3>`;
+    } else {
+        document.getElementById("myRow").innerHTML = cartona;
+    }
 }
 
 function deleteProduct(deletedIndex) {
     productList.splice(deletedIndex, 1)
     localStorage.setItem("products", JSON.stringify(productList));
-    display();
+    display(productList);
+}
+
+function searchProduct() {
+    var word = searchInput.value;
+    var searchedProducts = [];
+    for (var i = 0; i < productList.length; i++) {
+        if (productList[i].code.toLowerCase().includes(word.toLowerCase())) {
+            searchedProducts.push(productList[i]);
+        }
+    }
+    display(searchedProducts)
 }
 
 function clearInputs() {
